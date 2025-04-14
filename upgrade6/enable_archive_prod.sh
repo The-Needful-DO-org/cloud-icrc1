4b5275355af1899af0ce5b39cc516d6097b684e3
The Needful DO <the.needful.do@gmail.com>
2025-04-13 17:05:29 -0700

    Remove unneeded archive flag. Set archive controller to ledger controller. Increase archive memory.

--- upgrade6/enable_archive_prod.sh
+++ upgrade6/enable_archive_prod.sh
@@ -1,8 +1,8 @@
 dfx deploy icrc1-ledger --argument "( 
   variant { 
     Upgrade = opt record {
-      feature_flags = opt record{icrc2 = true; archive = true};
-      change_archive_options = opt record { num_blocks_to_archive = opt (1000 : nat64); trigger_threshold = opt (2000 : nat64); controller_id = opt principal \"pcj6u-uaaaa-aaaak-aewnq-cai\"; cycles_for_archive_creation = opt 10000000000000; };
+      feature_flags = opt record{icrc2 = true};
+      change_archive_options = opt record { num_blocks_to_archive = opt (1000 : nat64); trigger_threshold = opt (2000 : nat64); controller_id = opt principal \"uw2ky-lldfm-nyg4v-7qmqd-4fl7x-o4wzv-ah5dw-x75ao-ppqgx-xmu5s-nae\"; cycles_for_archive_creation = opt 10_000_000_000_000; node_max_memory_size_bytes = opt 3_221_225_472 };
     }
   }, 
 )" --mode upgrade --upgrade-unchanged --ic
